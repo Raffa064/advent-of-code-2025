26657db3c5ad58e65ffec4590e5a7f3a2eb7fca1
Raffa064 <raffaddtg@gmail.com>
2025-12-10 07:58:58 -0300

    Day1 - part2
    + Refactory

---
+++ day1/part1.py
@@ -0,0 +1,26 @@
+colors = {
+    "L": "\033[31m",  #
+    "R": "\033[34m",  #
+}
+
+with open("input.txt", "r") as file:
+    offset = 50
+    passwd = 0
+
+    for line in file.readlines():
+        line = line.strip()
+        dir = line[0]
+        step = int(line[1:])
+
+        print("% 8s %s%s \033[33m%d\033[0m" % (line, colors[dir], dir, step))
+
+        match dir:
+            case "L":
+                offset -= step
+            case "R":
+                offset += step
+
+        if offset % 100 == 0:
+            passwd += 1
+
+    print("Passwd is: ", passwd)
